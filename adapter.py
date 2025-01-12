dd3cb1592a6323851327f3d83bfa586e2933d2f1
andreisophie <andrei.maruntismm@gmail.com>
2025-01-12 14:21:02 +0200

    Many things work now

--- adapter.py
+++ adapter.py
@@ -1,19 +1,29 @@
 from time import sleep
 import paho.mqtt.client as mqtt
+import logging
 from influxdb import InfluxDBClient
 
 def on_message(client, userdata, message):
     # Print message to stdout
-    print(f"Received message '{message.payload.decode()}' on topic '{message.topic}'")
+    logging.info(f"Received message '{message.payload.decode()}' on topic '{message.topic}'")
 
 # Connect to InfluxDB
-# influxdbc = InfluxDBClient('influxdb', 8086)
-# print(influxdbc.get_list_database())
+influxdbc = InfluxDBClient('influxdb', 8086)
+logging.info(influxdbc.get_list_database())
 
-# Connect to MQTT broker
-mqttc = mqtt.Client()
-mqttc.on_message = on_message
+def main():
+    logging.info("Starting adapter...")
+    # Connect to MQTT broker
+    mqttc = mqtt.Client()
+    mqttc.on_message = on_message
 
-mqttc.connect("mqtt_broker", port=1883)
-mqttc.subscribe("#")
-mqttc.loop_forever()
+    mqttc.connect("mqtt_broker", port=1883)
+    mqttc.subscribe("#")
+    mqttc.loop_start()
+
+    while True:
+        logging.info("I am alive! Waiting for messages...")
+        sleep(5)
+
+if __name__ == "__main__":
+    main()
--- client.py
+++ client.py
@@ -1,14 +1,16 @@
 import paho.mqtt.client as mqtt
+from time import sleep
 
 mqttc = mqtt.Client()
 
-mqttc.connect(host="localhost")
+mqttc.connect(host="localhost", port=1883)
 print("Connected to MQTT broker succesfully")
 mqttc.loop_start()
 
 for i in range(10):
     mqttc.publish("tema3scd", "Hello, World!")
     print(f"Message sent: Hello, World! {i}")
+    sleep(1)
 
 mqttc.disconnect()
 mqttc.loop_stop()
