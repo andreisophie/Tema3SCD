196aad54561fd84caeba16d5b0d68fa1cba57b43
Andrei Maruntis <andrei.maruntismm@gmail.com>
2025-01-12 18:45:59 +0200

    Change stack name in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker swarm init
-docker build . -t tema3scd_adapter
-docker stack deploy -c stack.yml tema3scd
+docker build . -t scd3_adapter
+docker stack deploy -c stack.yml scd3
