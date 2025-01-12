f211db2ace5cd1dafcf81580879e978f7fc414ca
andreisophie <andrei.maruntismm@gmail.com>
2025-01-12 16:23:01 +0200

    Add data to influxdb functional

--- adapter.py
+++ adapter.py
@@ -1,29 +1,76 @@
 from time import sleep
 import paho.mqtt.client as mqtt
-import logging
 from influxdb import InfluxDBClient
+from re import match
+import json
+from datetime import datetime
 
-def on_message(client, userdata, message):
+def on_message(client, userdata, message):    
+    # Check that topic is valid
+    if not match(r"^[^/]+/[^/]+$", message.topic):
+        print(f"Invalid topic '{message.topic}', ignoring")
+        return
+    
     # Print message to stdout
-    logging.info(f"Received message '{message.payload.decode()}' on topic '{message.topic}'")
+    print(f"Received a message by topic [{message.topic}]")
+    
+    # Parse message
+    split_topic = message.topic.split("/")
+    location = split_topic[0]
+    station = split_topic[1]
+    
+    # Parse payload
+    parsed_payload = json.loads(message.payload.decode())
+    
+    # Parse timestamp separately
+    if "timestamp" in parsed_payload:
+        value = parsed_payload["timestamp"]
+        try:
+            timestamp = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S%z")
+        except ValueError:
+            modified_timestamp = value.rsplit(":", 1)[0] + value.rsplit(":", 1)[1]
+            timestamp = datetime.strptime(modified_timestamp, "%Y-%m-%dT%H:%M:%S%z")
+    else:
+        #  If timestamp is not provided, default is now
+        timestamp = datetime.now()
+    
+    data = []
+    for key in parsed_payload:
+        value = parsed_payload[key]
+        # Timestamp was parsed previously
+        if key == "timestamp":
+            continue
+        # Check whether value is a number
+        if not isinstance(value, (int, float)):
+            continue
+        # Create data point
+        data.append({
+            "measurement": f"{station}.{key}",
+            "tags": { "location": location, "station": station },
+            "time": timestamp,
+            "fields": { "value": value }
+        })
+        print(f"{location}.{station}.{key} {value}")
+
+    # Write data to InfluxDB
+    influxdbc.write_points(data)
+    print(f"Written data to InfluxDB")
+        
+            
+print("Starting adapter...")
 
 # Connect to InfluxDB
 influxdbc = InfluxDBClient('influxdb', 8086)
-logging.info(influxdbc.get_list_database())
+databases = influxdbc.get_list_database()
+print("Databases: ", databases)
+if "tema3scd" not in [db["name"] for db in databases]:
+    influxdbc.create_database("tema3scd")
+influxdbc.switch_database("tema3scd")
 
-def main():
-    logging.info("Starting adapter...")
-    # Connect to MQTT broker
-    mqttc = mqtt.Client()
-    mqttc.on_message = on_message
+# Connect to MQTT broker
+mqttc = mqtt.Client()
+mqttc.on_message = on_message
 
-    mqttc.connect("mqtt_broker", port=1883)
-    mqttc.subscribe("#")
-    mqttc.loop_start()
-
-    while True:
-        logging.info("I am alive! Waiting for messages...")
-        sleep(5)
-
-if __name__ == "__main__":
-    main()
+mqttc.connect("mqtt_broker", port=1883)
+mqttc.subscribe("#")
+mqttc.loop_forever()
--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 from time import sleep
+import json
 
 mqttc = mqtt.Client()
 
@@ -7,10 +8,19 @@ mqttc.connect(host="localhost", port=1883)
 print("Connected to MQTT broker succesfully")
 mqttc.loop_start()
 
-for i in range(10):
-    mqttc.publish("tema3scd", "Hello, World!")
-    print(f"Message sent: Hello, World! {i}")
-    sleep(1)
+topic = "UPB/RPi_1"
+
+payload = {
+    "BAT": 99,
+    "HUMID" : 40,
+    "PRJ" : "SCD",
+    "TMP" : 25.3,
+    "status": "OK" ,
+    "timestamp" : "2024-11-26T03:54:20+03:00"
+}
+
+mqttc.publish(topic, payload=json.dumps(payload))
+print(f"Published message to topic {topic}")
 
 mqttc.disconnect()
 mqttc.loop_stop()
